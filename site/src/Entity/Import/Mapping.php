46d2310243639e74084f8148405f7571d284a923
pavel_novikov <pavelsur07@gmail.com>
2023-04-05 19:33:06 +0300

    Product, Mapping added property

--- site/src/Entity/Import/Mapping.php
+++ site/src/Entity/Import/Mapping.php
@@ -6,4 +6,10 @@ namespace App\Entity\Import;
 
 class Mapping
 {
+    private int $id;
+    private string $type;
+    private string $externalAttributeName;
+    private string $externalVariantName;
+    private ?int $attributeId = null;
+    private ?int $variantId = null;
 }
--- site/src/Entity/Import/Product.php
+++ site/src/Entity/Import/Product.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 namespace App\Entity\Import;
 
+use App\Entity\Seller\Seller;
+
 class Product
 {
+    private int $id;
+    private Seller $seller;
+    private string $type;
+    private string $name;
+    private array $rawData = [];
 }
